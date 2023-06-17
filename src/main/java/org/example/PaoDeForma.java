1df39b92a868d6f90de8bf580729ccbb174f11de
felipefigfaria <feeh.faria.98@gmail.com>
2023-06-17 16:51:22 -0300

    commit

---
+++ src/main/java/org/example/PaoDeForma.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class PaoDeForma extends Pao{
+    public String verificarPromocao(){
+        if(this.calcularPreco() >= 5){
+            return "promocao ativa.";
+
+        }else{
+            return "promocao nao ativa.";
+        }
+
+    }
+
+    @Override
+    public String getTipo(){
+        return "Pao de forma";
+    }
+
+
+}
---
+++ src/main/java/org/example/PaoDoce.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class PaoDoce extends Pao{
+    public String verificarPromocao(){
+        if(this.calcularPreco() >= 4){
+            return "promocao ativa.";
+
+        }else{
+            return "promocao nao ativa.";
+        }
+
+    }
+
+
+    @Override
+    public String getTipo(){
+        return "Pao doce";
+    }
+
+}
